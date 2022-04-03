e2dad90d81846c96e6ef48dcc92042e993f1a5d0
DiegoAlexandrev <diego.alexandrev@gmail.com>
2022-04-03 09:40:12 -0300

    FrontEnd

---
+++ android/app/src/main/kotlin/com/example/streaming1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.streaming1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/app_module.dart
@@ -0,0 +1,15 @@
+/*
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'features/auth/auth_module.dart';
+
+class AppModule extends Module {
+  @override
+  List<Bind> get binds => [];
+
+  @override
+  List<ModularRoute> get routes => [
+        ModuleRoute('/', module: AuthModule()),
+      ];
+}
+*/
